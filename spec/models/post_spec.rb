73ddd6ae9aa2f242d99ba49905aaa1b1fb703d26
Sigita <sigita.zeb92@gmail.com>
2021-12-08 15:33:00 +0000

    Comments controller In progress

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { is_expected.to be }
+
+  it 'has a message' do
+    post = Post.new(message: "")
+    expect(post).to_not be_valid
+    post.message = "This is a message"
+    expect(post).to be_valid
+  end
 end
