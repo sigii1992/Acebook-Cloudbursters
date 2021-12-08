eb8657a0f2025c65be3e84519e0e64b3f8a526f6
Sigita <sigita.zeb92@gmail.com>
2021-12-08 16:43:07 +0000

    Display comments-missing tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { is_expected.to be }
 
-  # it 'has a body' do
-  #   post = Post.create(message: "This is a message")
-  #   p post
-  #   comment = Comment.create(body: "", post: post.id)
-  #   p comment
-  #   expect(comment).to_not be_valid
-  #   comment.body = "This is a body"
-  #   expect(comment).to be_valid
-  # end
+  it 'has a body' do
+    # post = Post.create(message: "This is a message")
+    # p post
+    # comment = Comment.create(body: "", post: post.id)
+    # p comment
+    # expect(comment).to_not be_valid
+    # comment.body = "This is a body"
+    # expect(comment).to be_valid
+  end
 
   # it "creates a comment" do
   #   post = Post.create(message: "This is a message")
