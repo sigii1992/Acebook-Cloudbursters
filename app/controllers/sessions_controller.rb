2c6ab8eaa31bed4873142c471fd3dee3c6ffdeaa
Catherine Young <Kate@Clunies-MacBook-Air.local>
2021-12-09 16:25:05 +0000

    Add feature test for login when not registered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
        sessions[:user_id] = @user.id
-      #  redirect_to '/homepage??'
+       redirect_to '/posts'
     else
        redirect_to '/log-in'
     end
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.feature "Login", type: :feature do
     scenario "A registered user can login to their accounts" do
+      # Set up test database to contain user: example@example.com 
       visit "/log-in"
       fill_in "Email", with: "example@example.com"
       fill_in "Password", with: "password"
       click_button "Login"
       expect(page).to have_content("New post") # redirects to /posts page
+    end
+
+    scenario "An unregistered user will be redirected to hte signup page" do
+      visit "/log-in"
+      fill_in "Email", with: "example@example.com"
+      fill_in "Password", with: "password"
+      click_button "Login"
+      expect(page).to have_content("Signup") # redirects to /signup page
     end
 end
