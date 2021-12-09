d6b91ee6cae74afc4d90e1e4351fb2b531490565
bmargerison <bmargerison@outlook.com>
2021-12-09 11:16:08 +0000

    Comment model/unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { is_expected.to be }
 
-  # Fix this test!!!!
-  it 'has a body' do
+  it 'must have a body to create new instance' do
     post = Post.create(message: "This is a message")
-    p post
-    comment = post.comments.create(body: "")
-    p comment
+    comment = Comment.create(body: "", post: post)
     expect(comment).to_not be_valid
-    comment.body = "This is a body"
+  end
+
+  it 'has a body instance variable' do
+    post = Post.create(message: "This is a message")
+    comment = Comment.create(body: "This is a body", post: post)
     expect(comment).to be_valid
-    p post.comments
   end
 end
