3a9cba5511e00f90acd513799085db2906417374
Dylan James <djame011@gold.ac.uk>
2021-12-09 11:11:11 +0000

    Users database added, controller for Sign-Ups added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'sign_ups', to: 'sign_ups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :posts
 end
