c52754a426f3590340f86d4bd5b3e9a80395cf75
Dylan James <djame011@gold.ac.uk>
2021-12-09 17:18:12 +0000

    Sign-up form working in feature test but not on browser

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,4 +1,20 @@
 class SignUpsController < ApplicationController
-  def index
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to posts_url, notice: "Successfully created account!"
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- spec/requests/sign_ups_spec.rb
+++ spec/requests/sign_ups_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "SignUps", type: :request do
-  describe "GET /index" do
+  describe "GET /" do
     it "returns http success" do
-      get "/sign_ups/index"
+      get "/sign_ups"
       expect(response).to have_http_status(:success)
     end
   end
