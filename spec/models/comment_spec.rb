0c33181d6353559d8bf4809a2c7d78dfc670c963
Sigita <sigita.zeb92@gmail.com>
2021-12-09 10:32:09 +0000

    Fix bug for displaying a comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe Comment, type: :model do
   it 'has a body' do
     post = Post.create(message: "This is a message")
     p post
-    comment = Comment.create(body: "", post: post.id)
+    comment = post.comments.create(body: "")
     p comment
     expect(comment).to_not be_valid
     comment.body = "This is a body"
     expect(comment).to be_valid
+    p post.comments
   end
 end
