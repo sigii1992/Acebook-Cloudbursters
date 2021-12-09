ffdfb87f38891b333eef0beba792ea990efa194b
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-12-09 14:12:06 +0000

    added forms and create method into session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       sessions[:user_id] = @user.id
+      #  redirect_to '/homepage??'
+    else
+       redirect_to '/log-in'
+    end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "homepage#index"
   get 'sign_ups', to: 'sign_ups#index'
   get '/log-in', to: 'sessions#new'
+  post '/log-in', to: 'sessions#create'
   get '/', to: 'sessions#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
