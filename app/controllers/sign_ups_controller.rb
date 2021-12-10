7ccddf7407fc8586fc6820204e13645f2d5a3a26
Dylan James <djame011@gold.ac.uk>
2021-12-10 11:16:20 +0000

    Sign-up form working in tests and browser

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,4 +1,5 @@
 class SignUpsController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -6,7 +7,7 @@ class SignUpsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to posts_url, notice: "Successfully created account!"
+      redirect_to root_path, notice: "Successfully created account!"
     else
       render :new
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a email to create new user' do
+    user = User.create(email: "", password: "password", password_confirmation: "password")
+    expect(user).to_not be_valid
+  end
+
+  it 'must have a body to create new instance' do
+    user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+    expect(user).to be_valid
+  end
+  
+  it 'must have a body to create new instance' do
+    user = User.create(email: "test@test.com", password: "password", password_confirmation: "assword")
+    expect(user).to_not be_valid
+  end
 end
