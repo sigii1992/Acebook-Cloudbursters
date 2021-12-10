c8eb81c40043c24b0f0134f8789cdfa359a98f6a
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-12-10 07:55:48 +0000

    added more

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   resources :users, only: [:new, :create]
   resources :posts do
     resources :comments
