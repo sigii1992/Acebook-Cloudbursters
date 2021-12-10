33088401bbea2dcdc608915eb2aa429546fb9f54
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-12-10 07:48:06 +0000

    homepage/login functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
   resources :posts do
     resources :comments
+
+    resources :users, only: [:new, :create]
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    get 'welcome', to: 'sessions#welcome'
+    get 'authorized', to: 'sessions#page_requires_login'
+
   end
 end
