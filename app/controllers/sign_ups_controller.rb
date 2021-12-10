d2125b6c1994ced77516883f1ecf5065e5b05e15
Dylan James <djame011@gold.ac.uk>
2021-12-10 12:25:17 +0000

    User Sign-In and being Logged In working

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -7,7 +7,8 @@ class SignUpsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Successfully created account!"
+      session[:user_id] = @user.id
+      redirect_to root_path, :flash => { :notice => "Successfully created account!" } 
     else
       render :new
     end
