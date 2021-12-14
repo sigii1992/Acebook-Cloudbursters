f8092bd5d9e01f7bd0156252174ddd02f6997648
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-12-14 11:31:45 +0000

    navbar sorted

---
+++ react-frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component=
+        {Home} />
+      </Routes>
+
+    </Router>
+    </>
+  );
+}
+
+export default App;
