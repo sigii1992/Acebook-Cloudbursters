0766d2428b76a4f65aaa77b9dc613166381243cd
Dylan James <djame011@gold.ac.uk>
2021-12-14 15:01:13 +0000

    WIP for comments to users and posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
+    # @user = User.find_by(id: session[:user_id])
+    # @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    # @comment.user_id = set_current_user.id
     redirect_to post_path(@post)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :post_id, :user_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
