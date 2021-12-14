cb773ca46f26a01d7f58bdee2d1ce6d747a2856c
Mark McLeod <markmcleod45@gmail.com>
2021-12-14 16:32:42 +0000

    Refactor to remove redundant flash notices

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,12 @@ class LikesController < ApplicationController
   before_action :find_like, only: [:destroy]
 
   def create
-    if already_liked?
-      flash[:notice] = "You can't like more than once"
-    else
-      @post.likes.create(user_id: set_current_user.id)
-    end
+    @post.likes.create(user_id: set_current_user.id)
     redirect_to post_path(@post.id)
   end
 
   def destroy
-    if !(already_liked?)
-      flash[:notice] = "You can't unlike a post you haven't liked yet"
-    else
-      @like.destroy
-    end
+    @like.destroy
     redirect_to post_path(@post.id)
   end
 
@@ -28,10 +20,5 @@ class LikesController < ApplicationController
 
   def find_like
     @like = @post.likes.find(params[:id])
-  end
-
-  def already_liked?
-    Like.where(user_id: set_current_user.id, post_id:
-    params[:post_id]).exists?
   end
 end
