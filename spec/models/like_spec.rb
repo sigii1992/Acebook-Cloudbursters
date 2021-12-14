2dd6c90b03cee9e92533e54241da427b8594f409
Sigita <sigita.zeb92@gmail.com>
2021-12-14 12:18:15 +0000

    Add feature and model test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a like' do
+  user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+  post = Post.create(message: "This is a message", user: user)
+  like = Like.create(user: user, post: post)
+  expect(like).to be_valid
+  end
+
+  it 'has a user and a post' do
+    user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+    post = Post.create(message: "This is a message", user: user)
+    like = Like.create(user: user, post: post)
+    expect(like.user).to be(user)
+    expect(like.post).to be(post)
+  end
 end
