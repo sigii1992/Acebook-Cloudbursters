2bc41c451ed95a0a5e9543d28255a2d7b38f4c9d
Sigita <sigita.zeb92@gmail.com>
2021-12-14 15:56:20 +0000

    You can see who commented the post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,8 @@ class CommentsController < ApplicationController
     # @user = User.find_by(id: session[:user_id])
     # @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comment.user_id = set_current_user.id
+    @comment.save
     # @comment.user_id = set_current_user.id
     redirect_to post_path(@post)
   end
