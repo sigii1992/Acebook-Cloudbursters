264d76cc7fe68433cb88fed91c5417054f4d7544
Sigita <sigita.zeb92@gmail.com>
2021-12-14 16:45:50 +0000

    Comments belong to a user, user email adress displayed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,9 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    # @user = User.find_by(id: session[:user_id])
-    # @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user_id = set_current_user.id
     @comment.save
-    # @comment.user_id = set_current_user.id
     redirect_to post_path(@post)
   end
 
