f8240ce9d66f63d712e67c1f61940cc5592e5c5e
Dylan James <djame011@gold.ac.uk>
2021-12-14 17:12:26 +0000

    Feature and model tests for comments

--- spec/features/user_can_submit_comment_spec.rb
+++ spec/features/user_can_submit_comment_spec.rb
@@ -16,4 +16,20 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Create Comment"
     expect(page).to have_content("This is a comment")
   end
+
+  scenario "user email is displayed next to comment" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "user[email]", with: "feature@test.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    click_button "Sign-up"
+    click_link "Add post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "Hello, world!"
+    fill_in "comment[body]", with: "This is a comment"
+    click_button "Create Comment"
+    expect(page).to have_content("Comment from: feature@test.com")
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Comment, type: :model do
     comment = Comment.create(body: "This is a body", post: post, user: user)
     expect(comment).to be_valid
   end
+
+  it 'has a body instance variable' do
+    user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+    post = Post.create(message: "This is a message", user: user)
+    comment = Comment.create(body: "This is a body", post: post, user: user)
+    expect(comment.post).to be(post)
+    expect(comment.user).to be(user)
+  end
 end
