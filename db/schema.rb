66f3654b2ee50923c4ac342a9c2a6e0fb2883963
Dylan James <djame011@gold.ac.uk>
2021-12-14 10:57:24 +0000

    Display post if user trys to like post more than once

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_172840) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
