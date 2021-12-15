b7b0e69d287a75b22e5d1aea2cc943f1003729d5
Mark McLeod <markmcleod45@gmail.com>
2021-12-15 17:41:44 +0000

    Comments controller tests in progress

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,21 +1,18 @@
-=begin
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  before do
-    @post = Post.create(message: "This is a post")
-  end
 
   describe "POST /" do
     it "responds with 200" do
-      post :create, params: { post_id: @post.id, comment: { body: "this is a comment", post_id: @post.id } }
-      expect(response).to redirect_to("/posts/#{@post.id}")
+      user = User.create(email: "example@example.com", password: "password", password_confirmation: "password")
+      post1 = Post.create(message: "This is a post", user_id: user.id)
+      #post :create, params: { comment: { body: "this is a comment" } }, post_id: { post_id: post1.id }
+      # expect(response).to redirect_to("/posts/#{@post.id}")
     end
 
-    it "creates a comment" do
-      post :create, params: { post_id: @post.id, comment: { body: "this is another comment", post_id: @post.id } }
-      expect(Comment.find_by(body: "this is another comment")).to be
-    end
+    # it "creates a comment" do
+    #   post :create, params: { post_id: @post.id, comment: { body: "this is another comment", post_id: @post.id } }
+    #   expect(Comment.find_by(body: "this is another comment")).to be
+    # end
   end
 end
-=end
