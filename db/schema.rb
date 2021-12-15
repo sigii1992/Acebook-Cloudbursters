d6907fc7402bb2684286844a58e3e24bf29cbae8
roshanabey2 <roshanabey2@gmail.com>
2021-12-15 12:13:12 +0000

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_151253) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
