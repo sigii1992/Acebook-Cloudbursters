0dbbb0560137946f80a94f8481436c460214df19
Dylan James <djame011@gold.ac.uk>
2021-12-15 17:14:08 +0000

    Posts controller test passing

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  # describe "POST /" do
-  #   it "responds with 200" do
-  #     User.create(email: "example@example.com", password: "password", password_confirmation: "password")
-  #     p User.last
-  #     post posts_url(User.last), params: { post: { message: "Hello, world!" } }
-  #     expect(response).to redirect_to(posts_url)
-  #   end
+  describe "POST /" do
+    it "responds with 200" do
+      user = User.create(email: "example@example.com", password: "password", password_confirmation: "password")
+      post :create, params: { post: { message: "Hello, world!" } }, session: { user_id: user.id } 
+      expect(response).to redirect_to(posts_url)
+    end
 
-    # it "creates a post" do
-    #   post :create, params: { post: { message: "Hello, world!" } }
-    #   expect(Post.find_by(message: "Hello, world!")).to be
-    # end
-  # end
+    it "creates a post" do
+      user = User.create(email: "example@example.com", password: "password", password_confirmation: "password")
+      post :create, params: { post: { message: "Hello, world!" } }, session: { user_id: user.id } 
+      expect(Post.find_by(message: "Hello, world!")).to be
+    end
+  end              
 
   describe "GET /" do
     it "responds with 200" do
