34b0b2a77fed98791ba8b94f4d2342523bb8617a
Mark McLeod <markmcleod45@gmail.com>
2021-12-16 12:27:16 +0000

    Deleted unneccessary files and folders. Add additional controller test for unsuccessful sign up

--- spec/controllers/sign_ups_controller_spec.rb
+++ spec/controllers/sign_ups_controller_spec.rb
@@ -9,9 +9,14 @@ RSpec.describe SignUpsController, type: :controller do
   end
 
   describe "POST /" do
-    it "responds with 200" do
+    it "responds with 200 when successfully signing up" do
       post :create, params: { user: { email: "example@example.com", password: "password", password_confirmation: "password" } }
       expect(response).to redirect_to(posts_url)
+    end
+
+    it "responds with 200 when sign up not successful" do
+      post :create, params: { user: { email: "example@example.com", password: "password", password_confirmation: "passwo1234" } }
+      expect(response).to have_http_status(200)
     end
 
     it "creates a user" do
