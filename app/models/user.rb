c90596741a12e0b9e1fe60f37ad31d17cd828372
roshanabey2 <roshanabey2@gmail.com>
2021-12-16 11:51:34 +0000

    Profile pic is now normal size  for sake of unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_one_attached :avatar do |attachable|
-        attachable.variant :thumb, resize: "100x100"
-      end
+    has_one_attached :avatar # do |attachable|
+        #attachable.variant :thumb, resize: "200x200"
+      #end
     has_many :posts
     has_secure_password
     validates :email, presence: true
