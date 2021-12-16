a7ae40366ef3442f17ed93928580bd97b5e1b867
Dylan James <djame011@gold.ac.uk>
2021-12-16 11:53:07 +0000

    Fixed tests for comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe CommentsController, type: :controller do
     it "responds with 200" do
       user = User.create(email: "example@example.com", password: "password", password_confirmation: "password")
       post1 = Post.create(message: "This is a post", user_id: user.id)
-      p user
-      p post1
-      #post :create, params: { comment: { body: "this is a comment" } }, post_id: { post_id: post1.id }
-      # expect(response).to redirect_to("/posts/#{@post.id}")
+      post :create, params: { comment: { body: "this is a comment" }, post_id: post1.id }, session: { user_id: user.id }
+      expect(response).to redirect_to("/posts/#{post1.id}")
     end
 
-    # it "creates a comment" do
-    #   post :create, params: { post_id: @post.id, comment: { body: "this is another comment", post_id: @post.id } }
-    #   expect(Comment.find_by(body: "this is another comment")).to be
-    # end
+    it "creates a comment" do
+      user = User.create(email: "example@example.com", password: "password", password_confirmation: "password")
+      post1 = Post.create(message: "This is a post", user_id: user.id)
+      post :create, params: { comment: { body: "this is another comment" }, post_id: post1.id }, session: { user_id: user.id }
+      expect(Comment.find_by(body: "this is another comment")).to be
+    end
   end
 end
