5f1657f8383ae8baf133cb1dba824c6c72a3fda9
bmargerison <bmargerison@outlook.com>
2021-12-16 11:43:33 +0000

    Updated API call to use React syntax

--- react-frontend/src/components/Posts.js
+++ react-frontend/src/components/Posts.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
 import '../App.css';
 import './Posts.css';
 
-function Posts() {
-  fetch('http://localhost:3000/api/v1/posts')
-  .then(response => response.json())
-  .then(response => {
-    response.data.forEach(post => {
-      const posts = document.createElement('div')
-      posts.classList.add('posts')
+const Posts = () => {
+  const [posts, setData] = useState([]);
 
-      const post_message = document.createElement('P')
-      post_message.innerText = post.message
-      post_message.classList.add('post_message')
+  useEffect(() => {
+    const url = "http://localhost:3000/api/v1/posts";
 
-      const created_at = document.createElement('P')
-      created_at.innerText = 'created at' + ' ' + post.created_at.slice(0,10) + ' ' + post.created_at.slice(11,16)
-     
-      posts.append(post_message)
-      posts.append(document.createElement('br'))
-      posts.append(created_at)
-      document.body.append(posts)
-  })
-});
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json.data);
+        setData(json.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div class="container">
-
+    <div class='all_posts'>
+      {posts.map((post, index) => (
+        <div class='posts'>
+        <p class='post_message'>{post.message}</p>
+        <br></br>
+        <p class='post_created_at'>{'created at' + ' ' + post.created_at.slice(0,10) + ' ' + post.created_at.slice(11,16)}</p>
+        </div>
+      ))}
     </div>
-    )
-}
+  );
+};
 
 export default Posts;
 
